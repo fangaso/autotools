96b430120f0812c3d7bc6419fdcd4796df155324
fangaso <61532453+fangaso@users.noreply.github.com>
2021-02-14 21:28:01 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,1 +1,18 @@
-curl -i https://git.io -F "url=https://ca3w.github.io/" -F "code=ca3w
+#!/bin/bash
+echo "1. China"
+echo "2. USA"
+echo "3. England"
+read -p "Please choose your country: " i
+case "$i" in
+1)
+echo "Your country is China."
+;;
+2)
+echo "Your country is USA."
+;;
+3)
+echo "Your country is England."
+;;
+*)
+echo "Please choose a right item."
+esac
