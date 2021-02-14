04a51a085f5e1c224a15a1f516cb471dad38d2fa
fangaso <61532453+fangaso@users.noreply.github.com>
2021-02-14 21:58:00 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -27,11 +27,29 @@ echo "3. 测试"
 read -p "请选择对应序号：" i
 case "$i" in
 1)
-echo "这是一个说明"
+#功能1列表
+echo "Cloudog Auto tools - 云狗工具箱"
+echo "1. 搭建-v2ray"
+echo "2. 搭建-宝塔面板"
+echo "3. 测试"
+read -p "请选择对应序号：" i
+case "$i" in
+1)
+bash <(curl -s -L https://git.io/v2ray.sh)
 ;;
+2)
+curl -sSO http://download.bt.cn/install/install_panel.sh && bash install_panel.sh
+;;
+3)
+echo "这是一个支持"
+;;
+*)
+;;
+#功能2列表
 2)
 echo "这是一个关于"
 ;;
+#功能3列表
 3)
 echo "这是一个支持"
 ;;
