3abe8d67f8354117a7b628b0e2b087861ded56b9
fangaso <61532453+fangaso@users.noreply.github.com>
2021-02-14 21:37:02 +0800

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-echo "1. China"
-echo "2. USA"
-echo "3. England"
-read -p "Please choose your country: " i
+echo "Cloudog Auto tools - 云狗工具箱"
+echo "1. 说明"
+echo "2. 关于"
+echo "3. 支持"
+echo "如不需要查阅相关内容请直接回车"
+read -p "请选择对应序号：" i
 case "$i" in
 1)
-echo "Your country is China."
+echo "这是一个说明"
 ;;
 2)
-echo "Your country is USA."
+echo "这是一个关于"
 ;;
 3)
-echo "Your country is England."
+echo "这是一个支持"
 ;;
 *)
-echo "Please choose a right item."
-bash <(curl -s -L https://git.io/cloudog)
+echo ""
+esec
